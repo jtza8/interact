fbc9cc3a601bf7fdf2b50f7c31b7e3c0ca1d9901
Jens Thiede <jensthiede@gmail.com>
2010-12-14 23:38:47 +0200

    Added function RENDER-FONT for font rendering to texture.

---
+++ click/font_tools.lisp
@@ -0,0 +1,24 @@
+; Copyright 2009 Jens Thiede. All rights reserved.
+; Use of this source code is governed by a BSD-style
+; license that can be found in the license.txt file
+; in the root directory of this project.
+
+(in-package :click)
+
+(defun render-text (text &key (type-face sdl:*default-font*)
+                    (justify "left") (colour sdl:*black*))
+  (let ((width (sdl:get-font-size text :size :w :font type-face))
+        (height (sdl:get-font-size text :size :h :font type-face)))
+    (sdl:with-surface (surface
+                       (sdl:create-surface width height :bpp 32 :pixel-alpha t))
+      (sdl:draw-string-blended-* text 0 0
+                                 :justify justify :surface surface
+                                 :font type-face :color colour)
+      (sdl-base::with-pixel (pix (sdl:fp surface))
+        (make-instance 'image-sprite
+                       :texture (gl:tex-image-2d :texture-2d 0 :rgba
+                                                 width height 0
+                                                 :rgba :unsigned-byte 
+                                                 (sdl-base::pixel-data pix))
+                       :width width
+                       :height height)))))
