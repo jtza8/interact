52236791a006bfc098704048fb3953ea8c2cdf61
Jens Thiede <jensthiede@gmail.com>
2011-01-24 14:04:01 +0200

    Renamed the base directory of the GUI components.

--- click/widget.lisp
+++ click/widget.lisp
@@ -1,4 +1,4 @@
-; Copyright 2009 Jens Thiede. All rights reserved.
+; Copyright 2011 Jens Thiede. All rights reserved.
 ; Use of this source code is governed by a BSD-style
 ; license that can be found in the license.txt file
 ; in the root directory of this project.
