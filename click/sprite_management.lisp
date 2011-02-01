cf4c5af92da45eeaa449fb990f7f8468666db71d
Jens Thiede <jensthiede@gmail.com>
2011-02-02 00:44:13 +0200

    Improved LOAD-TEXTURE-SPRITE. Adding support for solid colour sprites.

--- click/sprite_management.lisp
+++ click/sprite_management.lisp
@@ -14,7 +14,6 @@
 (defun load-texture-sprite (file)
   (il:with-bound-image (il:gen-image)
     (il:load-image (namestring file))
-    (il:convert-image :rgba :unsigned-byte)
     (il:check-error)
     (let ((img-mode (cffi:foreign-enum-keyword 
                      'il::data-format 
@@ -22,13 +21,35 @@
           (width (il:get-integer :image-width))
           (height (il:get-integer :image-height))
           (texture (car (gl:gen-textures 1))))
+      (if (eq img-mode :rgba)
+        (il:convert-image :rgba :unsigned-byte)
+        (il:convert-image :rgb :unsigned-byte))
+      (il:check-error)
       (gl:bind-texture :texture-2d texture)
       (gl:tex-parameter :texture-2d :texture-min-filter :linear)
-      (gl:tex-image-2d :texture-2d 0 :rgba width height 0 img-mode
+      (gl:tex-image-2d :texture-2d 0 img-mode width height 0 img-mode
                        :unsigned-byte (il:get-data))
-      (il:check-error)
       (make-instance 'texture-sprite :texture texture :width width
                      :height height))))
+
+(defun parse-color-sprite (entry)
+  (assert (>= (length entry) 5) () "invalid entry")
+  (let ((path (car entry))
+        (color (subseq entry 1 5))
+        (rest (subseq entry 5)))
+    (print path)
+    (print color)
+    (print rest)))
+
+(defun parse-sprites (base-dir)
+  (let ((sprites-conf-path (merge-pathnames "sprites.conf" base-dir)))
+    (unless (cl-fad:file-exists-p sprites-conf-path)
+      (return-from parse-sprites))
+    (with-open-file (file sprites-conf-path)
+      (loop for line = (read file nil 'end-of-file)
+            until (eq line 'end-of-file)
+            do (parse-color-sprite line)))))
+      
 
 (defun make-sprite-tree (base-dir)
   (flet ((make-keyword (string)
