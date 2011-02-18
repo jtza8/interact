f89454100631e3cc0e12a56181a2efd997fab8d8
Jens Thiede <jensthiede@gmail.com>
2011-02-18 13:05:17 +0200

    Changed source-file headers and copyright information.

--- click/listener.lisp
+++ click/listener.lisp
@@ -1,5 +1,4 @@
 ; Use of this source code is governed by a BSD-style
-; Copyright 2009 Jens Thiede. All rights reserved.
 ; license that can be found in the license.txt file
 ; in the root directory of this project.
 
--- click/sprite.lisp
+++ click/sprite.lisp
@@ -1,4 +1,3 @@
-; Copyright 2009 Jens Thiede. All rights reserved.
 ; Use of this source code is governed by a BSD-style
 ; license that can be found in the license.txt file
 ; in the root directory of this project.
