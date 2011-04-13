f7b73cac5629a4274ab1ded6856e87c0e4da8496
Jens Thiede <jensthiede@gmail.com>
2011-04-13 14:57:57 +0200

    Updated the exported symbols of Click.

--- click/package.lisp
+++ click/package.lisp
@@ -8,77 +8,111 @@
                 #:invalid-node
                 #:free
                 #:with-nodes)
-  (:export #:*root-container*
-           #:*settings*
-           #:*sprite-path*
-           #:*sprite-tree*
-           #:add-listener
-           #:add-container
-           #:activate-container
-           #:deactivate-container
-           #:add-igo
-           #:desire-events
-           #:draw
-           #:draw-at
-           #:event-data
-           #:event-type
-           #:sprite-node
-           #:sprite-node-from
-           #:init-click
-           #:start-display-system
-           #:invalid-event
-           #:invalid-event-type
-           #:invalid-sprite-node
-           #:listenable
-           #:listener
-           #:load-settings
-           #:provide-events
-           #:rectangle
-           #:remove-listener
-           #:remove-tag
-           #:remove-igo
-           #:render-text
-           #:reset-settings
-           #:run-display-system
-           #:container
-           #:container-manager
-           #:send-event
-           #:sprite
-           #:tag-error
-           #:tag-of
-           #:tag-igo
-           #:texture-sprite
-           #:theme
-           #:translate
-           #:undesire-events
-           #:undo-translate
-           #:igo
-           #:igo-of
-           #:with-event-keys
-           #:with-nodes
-           #:with-translate
-           #:within
+  (:export 
+   ; Variables
+   #:*clipping-depth*
+   #:*global-stopwatch*
+   #:*root-container*
+   #:*sprite-tree*
 
-           ; Accessors:
-           #:active-containers
-           #:background
-           #:desired-events
-           #:event
-           #:height
-           #:invalid-node
-           #:listeners
-           #:provided-events
-           #:reason
-           #:containers
-           #:texture
-           #:igos
-           #:width
-           #:windows
-           #:absolute-pos
-           #:absolute-x
-           #:absolute-y
-           #:x
-           #:y))
+   ; Functions
+   #:build-sprite-sheet
+   #:clip-display
+   #:event-data
+   #:event-type
+   #:image-to-sprite
+   #:list-image-file-sequence
+   #:load-image-sprite
+   #:load-sprite
+   #:load-sprite-path
+   #:load-sprite-sheet
+   #:prepare-click
+   #:quit-display-system
+   #:read-sheet-header
+   #:run-display-system
+   #:set-up-root-container
+   #:sprite-node
+   #:start-display-system
+   #:translate
+   #:undo-clipping
+   #:undo-translate
+   #:write-sheet-header
+
+   ; Methods
+   #:absolute-pos
+   #:absolute-x
+   #:absolute-y
+   #:add-igo
+   #:add-listener
+   #:desire-events
+   #:diverge
+   #:draw
+   #:draw-at
+   #:draw-background
+   #:handle-event
+   #:igo-of
+   #:initialize-instance
+   #:lap
+   #:map-input
+   #:provide-events
+   #:remove-igo
+   #:remove-listener
+   #:remove-tag
+   #:reset
+   #:select-handler
+   #:send-event
+   #:start
+   #:stop
+   #:tag-igo
+   #:tag-of
+   #:undesire-events
+   #:within
+
+   ; Accessors
+   #:addendum
+   #:background
+   #:clipping
+   #:color
+   #:desired-events
+   #:event
+   #:fps
+   #:height
+   #:igos
+   #:listeners
+   #:parent
+   #:provided-events
+   #:reason
+   #:texture
+   #:width
+   #:x
+   #:y
+
+   ; Macros
+   #:with-clipping
+   #:with-display-system
+   #:with-event-keys
+   #:with-image-sequence
+   #:with-translate
+
+   ; Classes
+   #:animation-sprite
+   #:color-sprite
+   #:container
+   #:event-converter
+   #:igo
+   #:listenable
+   #:listener
+   #:sprite
+   #:stopwatch
+   #:texture-sprite
+
+   ; Conditions
+   #:file-format-error
+   #:image-error
+   #:invalid-event
+   #:invalid-event-type
+   #:pixel-index-error
+   #:tag-error))
 
 (il:init)
 (ilu:init)
